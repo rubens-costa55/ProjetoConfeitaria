fa65b798815c876c32835a7a109dc9a8892727d3
Joseane Felinto <joseanefelintoalvesjo@gmail.com>
2026-03-29 15:14:15 -0300

    Atualizações no redefinir senha

---
+++ ProjetoConfeitaria/PrimeiraTela/RedefinirSenha.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace PrimeiraTela
+{
+    partial class FrmRedefinirSenha
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            PanelJanela = new Panel();
+            PanelDireito = new Panel();
+            label5 = new Label();
+            PicBola2 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            PicBola1 = new Label();
+            PanelRegras = new Panel();
+            lbRegrasTitulo = new Label();
+            lbRegra3 = new Label();
+            lbRegra2 = new Label();
+            lbRegra1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel3 = new Panel();
+            btnAtualizarSenha = new Button();
+            lbConfirmarSenha = new Label();
+            txtConfirmarSenha = new TextBox();
+            txtNovaSenha = new TextBox();
+            lbNovaSenha = new Label();
+            lbTitulo = new Label();
+            lbDescricao = new Label();
+            PanelJanela.SuspendLayout();
+            PanelDireito.SuspendLayout();
+            PanelRegras.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // PanelJanela
+            // 
+            PanelJanela.BackColor = Color.FromArgb(249, 245, 243);
+            PanelJanela.BorderStyle = BorderStyle.FixedSingle;
+            PanelJanela.Controls.Add(PanelDireito);
+            PanelJanela.Controls.Add(panel3);
+            PanelJanela.Location = new Point(12, 12);
+            PanelJanela.Name = "PanelJanela";
+            PanelJanela.Size = new Size(1300, 354);
+            PanelJanela.TabIndex = 0;
+            // 
+            // PanelDireito
+            // 
+            PanelDireito.BackColor = Color.Transparent;
+            PanelDireito.Controls.Add(label5);
+            PanelDireito.Controls.Add(PicBola2);
+            PanelDireito.Controls.Add(label2);
+            PanelDireito.Controls.Add(label1);
+            PanelDireito.Controls.Add(label4);
+            PanelDireito.Controls.Add(label3);
+            PanelDireito.Controls.Add(PicBola1);
+            PanelDireito.Controls.Add(PanelRegras);
+            PanelDireito.Controls.Add(lbRegra3);
+            PanelDireito.Controls.Add(lbRegra2);
+            PanelDireito.Controls.Add(lbRegra1);
+            PanelDireito.Controls.Add(pictureBox1);
+            PanelDireito.Location = new Point(724, 7);
+            PanelDireito.Name = "PanelDireito";
+            PanelDireito.Size = new Size(750, 615);
+            PanelDireito.TabIndex = 2;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.FromArgb(242, 143, 149);
+            label5.Location = new Point(45, 229);
+            label5.Name = "label5";
+            label5.Size = new Size(25, 40);
+            label5.TabIndex = 14;
+            label5.Text = ".";
+            // 
+            // PicBola2
+            // 
+            PicBola2.AutoSize = true;
+            PicBola2.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            PicBola2.ForeColor = Color.FromArgb(242, 143, 149);
+            PicBola2.Location = new Point(45, 190);
+            PicBola2.Name = "PicBola2";
+            PicBola2.Size = new Size(25, 40);
+            PicBola2.TabIndex = 13;
+            PicBola2.Text = ".";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(242, 143, 149);
+            label2.Location = new Point(387, 311);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 40);
+            label2.TabIndex = 12;
+            label2.Text = ".";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(242, 143, 149);
+            label1.Location = new Point(379, 303);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 40);
+            label1.TabIndex = 11;
+            label1.Text = ".";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.FromArgb(242, 143, 149);
+            label4.Location = new Point(371, 295);
+            label4.Name = "label4";
+            label4.Size = new Size(25, 40);
+            label4.TabIndex = 10;
+            label4.Text = ".";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(242, 143, 149);
+            label3.Location = new Point(363, 287);
+            label3.Name = "label3";
+            label3.Size = new Size(25, 40);
+            label3.TabIndex = 9;
+            label3.Text = ".";
+            // 
+            // PicBola1
+            // 
+            PicBola1.AutoSize = true;
+            PicBola1.Font = new Font("Segoe UI", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            PicBola1.ForeColor = Color.FromArgb(242, 143, 149);
+            PicBola1.Location = new Point(45, 156);
+            PicBola1.Name = "PicBola1";
+            PicBola1.Size = new Size(25, 40);
+            PicBola1.TabIndex = 6;
+            PicBola1.Text = ".";
+            // 
+            // PanelRegras
+            // 
+            PanelRegras.BackColor = Color.FromArgb(232, 174, 184);
+            PanelRegras.Controls.Add(lbRegrasTitulo);
+            PanelRegras.Location = new Point(63, 122);
+            PanelRegras.Name = "PanelRegras";
+            PanelRegras.Size = new Size(204, 34);
+            PanelRegras.TabIndex = 5;
+            // 
+            // lbRegrasTitulo
+            // 
+            lbRegrasTitulo.AutoSize = true;
+            lbRegrasTitulo.Font = new Font("Segoe UI", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbRegrasTitulo.ForeColor = Color.White;
+            lbRegrasTitulo.Location = new Point(25, 7);
+            lbRegrasTitulo.Name = "lbRegrasTitulo";
+            lbRegrasTitulo.Size = new Size(154, 20);
+            lbRegrasTitulo.TabIndex = 1;
+            lbRegrasTitulo.Text = "REGRAS SUGERIDAS";
+            lbRegrasTitulo.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lbRegra3
+            // 
+            lbRegra3.AutoSize = true;
+            lbRegra3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbRegra3.ForeColor = Color.FromArgb(126, 99, 92);
+            lbRegra3.Location = new Point(76, 248);
+            lbRegra3.Name = "lbRegra3";
+            lbRegra3.Size = new Size(185, 20);
+            lbRegra3.TabIndex = 4;
+            lbRegra3.Text = "Evite usar a senha anterior.";
+            lbRegra3.Click += label9_Click;
+            // 
+            // lbRegra2
+            // 
+            lbRegra2.AutoSize = true;
+            lbRegra2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbRegra2.ForeColor = Color.FromArgb(126, 99, 92);
+            lbRegra2.Location = new Point(76, 212);
+            lbRegra2.Name = "lbRegra2";
+            lbRegra2.Size = new Size(241, 20);
+            lbRegra2.TabIndex = 3;
+            lbRegra2.Text = "Misture letras, números e simbolos.";
+            // 
+            // lbRegra1
+            // 
+            lbRegra1.AutoSize = true;
+            lbRegra1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbRegra1.ForeColor = Color.FromArgb(126, 99, 92);
+            lbRegra1.Location = new Point(76, 175);
+            lbRegra1.Name = "lbRegra1";
+            lbRegra1.Size = new Size(205, 20);
+            lbRegra1.TabIndex = 2;
+            lbRegra1.Text = "Use pelo menos 8  caracteres.";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.LOGO__1__removebg_preview;
+            pictureBox1.Location = new Point(76, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(179, 104);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(235, 221, 218);
+            panel3.Controls.Add(btnAtualizarSenha);
+            panel3.Controls.Add(lbConfirmarSenha);
+            panel3.Controls.Add(txtConfirmarSenha);
+            panel3.Controls.Add(txtNovaSenha);
+            panel3.Controls.Add(lbNovaSenha);
+            panel3.Controls.Add(lbTitulo);
+            panel3.Controls.Add(lbDescricao);
+            panel3.Location = new Point(5, 7);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(700, 599);
+            panel3.TabIndex = 1;
+            // 
+            // btnAtualizarSenha
+            // 
+            btnAtualizarSenha.BackColor = Color.FromArgb(201, 142, 124);
+            btnAtualizarSenha.FlatStyle = FlatStyle.Flat;
+            btnAtualizarSenha.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnAtualizarSenha.ForeColor = Color.White;
+            btnAtualizarSenha.Location = new Point(35, 287);
+            btnAtualizarSenha.Name = "btnAtualizarSenha";
+            btnAtualizarSenha.RightToLeft = RightToLeft.No;
+            btnAtualizarSenha.Size = new Size(485, 52);
+            btnAtualizarSenha.TabIndex = 3;
+            btnAtualizarSenha.Text = "Atualizar Senha";
+            btnAtualizarSenha.UseVisualStyleBackColor = false;
+            btnAtualizarSenha.Click += btnAtualizarSenha_Click;
+            // 
+            // lbConfirmarSenha
+            // 
+            lbConfirmarSenha.AutoSize = true;
+            lbConfirmarSenha.BackColor = Color.FromArgb(235, 221, 218);
+            lbConfirmarSenha.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbConfirmarSenha.ForeColor = Color.FromArgb(185, 120, 103);
+            lbConfirmarSenha.Location = new Point(12, 175);
+            lbConfirmarSenha.Name = "lbConfirmarSenha";
+            lbConfirmarSenha.Size = new Size(138, 21);
+            lbConfirmarSenha.TabIndex = 7;
+            lbConfirmarSenha.Text = "Confirmar Senha";
+            // 
+            // txtConfirmarSenha
+            // 
+            txtConfirmarSenha.BackColor = Color.White;
+            txtConfirmarSenha.BorderStyle = BorderStyle.FixedSingle;
+            txtConfirmarSenha.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtConfirmarSenha.ForeColor = Color.FromArgb(191, 167, 157);
+            txtConfirmarSenha.Location = new Point(12, 212);
+            txtConfirmarSenha.Name = "txtConfirmarSenha";
+            txtConfirmarSenha.Size = new Size(485, 29);
+            txtConfirmarSenha.TabIndex = 6;
+            txtConfirmarSenha.Text = "Confirmar Senha";
+            txtConfirmarSenha.UseSystemPasswordChar = true;
+            // 
+            // txtNovaSenha
+            // 
+            txtNovaSenha.BackColor = Color.White;
+            txtNovaSenha.BorderStyle = BorderStyle.FixedSingle;
+            txtNovaSenha.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtNovaSenha.ForeColor = Color.FromArgb(191, 167, 157);
+            txtNovaSenha.Location = new Point(12, 127);
+            txtNovaSenha.Name = "txtNovaSenha";
+            txtNovaSenha.Size = new Size(485, 29);
+            txtNovaSenha.TabIndex = 5;
+            txtNovaSenha.Text = "Nova Senha";
+            txtNovaSenha.UseSystemPasswordChar = true;
+            // 
+            // lbNovaSenha
+            // 
+            lbNovaSenha.AutoSize = true;
+            lbNovaSenha.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbNovaSenha.ForeColor = Color.FromArgb(185, 120, 103);
+            lbNovaSenha.Location = new Point(12, 95);
+            lbNovaSenha.Name = "lbNovaSenha";
+            lbNovaSenha.Size = new Size(102, 21);
+            lbNovaSenha.TabIndex = 4;
+            lbNovaSenha.Text = "Nova Senha";
+            // 
+            // lbTitulo
+            // 
+            lbTitulo.AutoSize = true;
+            lbTitulo.Font = new Font("Segoe UI", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbTitulo.ForeColor = Color.FromArgb(201, 137, 120);
+            lbTitulo.Location = new Point(85, 5);
+            lbTitulo.Name = "lbTitulo";
+            lbTitulo.Size = new Size(255, 45);
+            lbTitulo.TabIndex = 0;
+            lbTitulo.Text = "Redefinir Senha";
+            lbTitulo.Click += label1_Click;
+            // 
+            // lbDescricao
+            // 
+            lbDescricao.AutoSize = true;
+            lbDescricao.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lbDescricao.ForeColor = Color.FromArgb(126, 99, 92);
+            lbDescricao.Location = new Point(12, 61);
+            lbDescricao.Name = "lbDescricao";
+            lbDescricao.Size = new Size(454, 20);
+            lbDescricao.TabIndex = 3;
+            lbDescricao.Text = "Atualize sua senha com segurança para continuar usando o sistema";
+            // 
+            // FrmRedefinirSenha
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(246, 239, 237);
+            ClientSize = new Size(1370, 749);
+            Controls.Add(PanelJanela);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "FrmRedefinirSenha";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "RedefinirSenha";
+            PanelJanela.ResumeLayout(false);
+            PanelDireito.ResumeLayout(false);
+            PanelDireito.PerformLayout();
+            PanelRegras.ResumeLayout(false);
+            PanelRegras.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel PanelJanela;
+        private Panel panel3;
+        private Label lbTitulo;
+        private Panel PanelDireito;
+        private Button btnAtualizarSenha;
+        private Label lbConfirmarSenha;
+        private TextBox txtConfirmarSenha;
+        private TextBox txtNovaSenha;
+        private Label lbNovaSenha;
+        private Label lbDescricao;
+        private PictureBox pictureBox1;
+        private Label lbRegra2;
+        private Label lbRegra1;
+        private Label lbRegrasTitulo;
+        private Label lbRegra3;
+        private Panel PanelRegras;
+        private Label PicBola1;
+        private Label label4;
+        private Label label3;
+        private Label label5;
+        private Label PicBola2;
+        private Label label2;
+        private Label label1;
+    }
+}
---
+++ ProjetoConfeitaria/PrimeiraTela/RedefinirSenha.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiraTela
+{
+    public partial class FrmRedefinirSenha : Form
+    {
+        public FrmRedefinirSenha()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAtualizarSenha_Click(object sender, EventArgs e)
+        {
+            if (txtNovaSenha.Text == "" || txtConfirmarSenha.Text == "")
+            {
+                MessageBox.Show("Preencha os dois campos.");
+                return;
+            }
+            if (txtNovaSenha.Text != txtConfirmarSenha.Text)
+            {
+                MessageBox.Show("As senhas não coincidem.");
+                return;
+            }
+            MessageBox.Show("Senha atualizada com sucesso!");
+            TelaLogin tela = new TelaLogin ();
+            tela.Show();
+
+            this.Hide();
+        }
+        
+    }
+    
+}
