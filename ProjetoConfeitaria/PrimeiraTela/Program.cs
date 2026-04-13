b1798d45c85d8b77806c5656f192557848fa87e6
Renata <Renata_Regina_567@hotmail.com>
2026-04-13 20:25:11 -0300

    Alterado erro

--- ProjetoConfeitaria/PrimeiraTela/Program.cs
+++ ProjetoConfeitaria/PrimeiraTela/Program.cs
@@ -11,23 +11,19 @@ namespace PrimeiraTela
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-<<<<<<< HEAD
+
            
             Application.Run(new PedidosAtuais());
-=======
-<<<<<<< HEAD
->>>>>>> 337c7f1f497a7211bf0cc3b9edd48e272fd64cf1
+
 
             Application.Run(new TelaLogin());
-=======
-<<<<<<< HEAD
             Application.Run(new TelaLogin());
-=======
+
            
             Application.Run(new FrmHistoricoPedidos());
->>>>>>> 9745e19ac948dbcb4811571851b75a7e3d534e5a
 
->>>>>>> cb312eecb9613f9be61e0059fc76d8f181760243
+
+
         }
     }
 }
