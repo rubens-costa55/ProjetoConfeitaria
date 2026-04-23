8a793ee7ea26aa1d50cc806e4fb77b6a61beb2e9
Rubens <rubenscostamch@gmail.com>
2026-04-22 21:57:46 -0300

    attcadastro

--- ProjetoConfeitaria/PrimeiraTela/CadastroProdutos.Designer.cs
+++ ProjetoConfeitaria/PrimeiraTela/CadastroProdutos.Designer.cs
@@ -28,22 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CadastroProdutos));
             panel1 = new Panel();
             panel2 = new Panel();
             label1 = new Label();
             label2 = new Label();
             panel3 = new Panel();
-            label3 = new Label();
-            textBox1 = new TextBox();
-            label4 = new Label();
             textBox2 = new TextBox();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            btnSairNA = new Button();
+            btnHistoricoNA = new Button();
+            btnPedidosAtuaisNA = new Button();
+            btnMenuNA = new Button();
+            panel4 = new Panel();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            button3 = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            panel1.SuspendLayout();
             panel2.SuspendLayout();
             panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
             SuspendLayout();
             // 
             // panel1
             // 
             panel1.BackColor = Color.FromArgb(239, 229, 226);
+            panel1.Controls.Add(pictureBox8);
+            panel1.Controls.Add(pictureBox7);
+            panel1.Controls.Add(pictureBox6);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Controls.Add(pictureBox2);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(btnSairNA);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(btnHistoricoNA);
+            panel1.Controls.Add(panel4);
+            panel1.Controls.Add(btnPedidosAtuaisNA);
+            panel1.Controls.Add(btnMenuNA);
             panel1.Location = new Point(30, 90);
             panel1.Name = "panel1";
             panel1.Size = new Size(260, 720);
@@ -93,23 +127,13 @@
             panel3.Size = new Size(760, 500);
             panel3.TabIndex = 3;
             // 
-            // label3
+            // textBox2
             // 
-            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.ForeColor = Color.FromArgb(201, 142, 124);
-            label3.Location = new Point(40, 40);
-            label3.Name = "label3";
-            label3.Size = new Size(200, 30);
-            label3.TabIndex = 0;
-            label3.Text = "Nome do Produto:";
-            // 
-            // textBox1
-            // 
-            textBox1.ForeColor = Color.FromArgb(111, 84, 75);
-            textBox1.Location = new Point(40, 73);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(650, 23);
-            textBox1.TabIndex = 1;
+            textBox2.ForeColor = Color.FromArgb(111, 84, 75);
+            textBox2.Location = new Point(40, 171);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(147, 23);
+            textBox2.TabIndex = 3;
             // 
             // label4
             // 
@@ -121,13 +145,180 @@
             label4.TabIndex = 2;
             label4.Text = "Preço unitário:";
             // 
-            // textBox2
+            // textBox1
             // 
-            textBox2.ForeColor = Color.FromArgb(111, 84, 75);
-            textBox2.Location = new Point(40, 171);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(147, 23);
-            textBox2.TabIndex = 3;
+            textBox1.ForeColor = Color.FromArgb(111, 84, 75);
+            textBox1.Location = new Point(40, 63);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(650, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.FromArgb(201, 142, 124);
+            label3.Location = new Point(40, 40);
+            label3.Name = "label3";
+            label3.Size = new Size(200, 30);
+            label3.TabIndex = 0;
+            label3.Text = "Nome do Produto:";
+            // 
+            // btnSairNA
+            // 
+            btnSairNA.BackColor = Color.FromArgb(247, 242, 241);
+            btnSairNA.FlatAppearance.BorderColor = Color.FromArgb(228, 206, 199);
+            btnSairNA.FlatStyle = FlatStyle.Flat;
+            btnSairNA.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnSairNA.ForeColor = Color.FromArgb(123, 97, 88);
+            btnSairNA.Location = new Point(16, 630);
+            btnSairNA.Name = "btnSairNA";
+            btnSairNA.Size = new Size(220, 70);
+            btnSairNA.TabIndex = 15;
+            btnSairNA.Text = "Sair";
+            btnSairNA.UseVisualStyleBackColor = false;
+            // 
+            // btnHistoricoNA
+            // 
+            btnHistoricoNA.BackColor = Color.FromArgb(247, 242, 241);
+            btnHistoricoNA.FlatAppearance.BorderColor = Color.FromArgb(228, 206, 199);
+            btnHistoricoNA.FlatStyle = FlatStyle.Flat;
+            btnHistoricoNA.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnHistoricoNA.ForeColor = Color.FromArgb(123, 97, 88);
+            btnHistoricoNA.Location = new Point(16, 433);
+            btnHistoricoNA.Name = "btnHistoricoNA";
+            btnHistoricoNA.Size = new Size(220, 70);
+            btnHistoricoNA.TabIndex = 14;
+            btnHistoricoNA.Text = "Histórico\r\nPedidos";
+            btnHistoricoNA.UseVisualStyleBackColor = false;
+            // 
+            // btnPedidosAtuaisNA
+            // 
+            btnPedidosAtuaisNA.BackColor = Color.FromArgb(247, 242, 241);
+            btnPedidosAtuaisNA.FlatAppearance.BorderColor = Color.FromArgb(228, 206, 199);
+            btnPedidosAtuaisNA.FlatStyle = FlatStyle.Flat;
+            btnPedidosAtuaisNA.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnPedidosAtuaisNA.ForeColor = Color.FromArgb(123, 97, 88);
+            btnPedidosAtuaisNA.Location = new Point(16, 344);
+            btnPedidosAtuaisNA.Name = "btnPedidosAtuaisNA";
+            btnPedidosAtuaisNA.Size = new Size(220, 70);
+            btnPedidosAtuaisNA.TabIndex = 13;
+            btnPedidosAtuaisNA.Text = "Pedidos Atuais";
+            btnPedidosAtuaisNA.UseVisualStyleBackColor = false;
+            // 
+            // btnMenuNA
+            // 
+            btnMenuNA.BackColor = Color.FromArgb(247, 242, 241);
+            btnMenuNA.FlatAppearance.BorderColor = Color.FromArgb(228, 206, 199);
+            btnMenuNA.FlatStyle = FlatStyle.Flat;
+            btnMenuNA.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnMenuNA.ForeColor = Color.FromArgb(123, 97, 88);
+            btnMenuNA.Location = new Point(16, 177);
+            btnMenuNA.Name = "btnMenuNA";
+            btnMenuNA.Size = new Size(220, 70);
+            btnMenuNA.TabIndex = 12;
+            btnMenuNA.Text = "Menu Principal";
+            btnMenuNA.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(216, 194, 187);
+            panel4.Location = new Point(16, 149);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(205, 2);
+            panel4.TabIndex = 10;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.LOGO__1__removebg_preview;
+            pictureBox1.Location = new Point(48, 10);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(150, 110);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(247, 242, 241);
+            button2.FlatAppearance.BorderColor = Color.FromArgb(228, 206, 199);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.FromArgb(123, 97, 88);
+            button2.Location = new Point(16, 259);
+            button2.Name = "button2";
+            button2.Size = new Size(220, 70);
+            button2.TabIndex = 16;
+            button2.Text = "Novo\r\nAgendamento";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(201, 142, 124);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(16, 529);
+            button3.Name = "button3";
+            button3.Size = new Size(220, 70);
+            button3.TabIndex = 17;
+            button3.Text = "Cadastro \r\nProdutos";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.FromArgb(247, 242, 241);
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(33, 199);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(33, 32);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 13;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.FromArgb(255, 253, 252);
+            pictureBox3.Image = Properties.Resources._1_removebg_preview;
+            pictureBox3.Location = new Point(32, 280);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(34, 29);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 18;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.BackColor = Color.FromArgb(247, 242, 241);
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(33, 364);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(33, 32);
+            pictureBox6.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox6.TabIndex = 19;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.BackColor = Color.FromArgb(247, 242, 241);
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(33, 458);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(33, 32);
+            pictureBox7.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox7.TabIndex = 20;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.BackColor = Color.FromArgb(247, 242, 241);
+            pictureBox8.Image = Properties.Resources._3_removebg_preview;
+            pictureBox8.Location = new Point(33, 650);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(33, 32);
+            pictureBox8.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox8.TabIndex = 21;
+            pictureBox8.TabStop = false;
             // 
             // CadastroProdutos
             // 
@@ -144,9 +335,16 @@
             Name = "CadastroProdutos";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Cadastro Produtos";
+            panel1.ResumeLayout(false);
             panel2.ResumeLayout(false);
             panel3.ResumeLayout(false);
             panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
             ResumeLayout(false);
         }
 
@@ -161,5 +359,18 @@
         private TextBox textBox2;
         private Label label4;
         private TextBox textBox1;
+        private Button button3;
+        private Button button2;
+        private Button btnSairNA;
+        private PictureBox pictureBox1;
+        private Button btnHistoricoNA;
+        private Panel panel4;
+        private Button btnPedidosAtuaisNA;
+        private Button btnMenuNA;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
     }
 }
